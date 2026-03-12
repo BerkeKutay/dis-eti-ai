3b3ba75d593933b8ebe6265dfdf9b9450e6e4f30
Berke Kutay Karakoç <karakocberkekutay@gmail.com>
2026-03-12 23:40:36 +0300

    download_models.py eklendi

--- download_models.py
+++ download_models.py
@@ -5,19 +5,9 @@ os.makedirs("runs/detect/dental_strong_m_v5/weights", exist_ok=True)
 os.makedirs("dental_llm_project/trained_model", exist_ok=True)
 
 print("YOLO modeli indiriliyor...")
-hf_hub_download(
-    repo_id="Kutay0/dis-eti-ai",
-    filename="yolo/best.pt",
-    local_dir=".",
-    repo_type="model"
-)
+hf_hub_download(repo_id="Kutay0/dis-eti-ai", filename="yolo/best.pt", local_dir=".", repo_type="model")
 
 print("LLM modeli indiriliyor...")
-hf_hub_download(
-    repo_id="Kutay0/dis-eti-ai",
-    filename="llm/dental_model.pt",
-    local_dir=".",
-    repo_type="model"
-)
+hf_hub_download(repo_id="Kutay0/dis-eti-ai", filename="llm/dental_model.pt", local_dir=".", repo_type="model")
 
 print("Modeller indirildi!")
